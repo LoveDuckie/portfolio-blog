f0b1a2443c9384e8f52d2a62bd058b838a710a90
Luc Shelton <lucshelton@gmail.com>
2022-05-16 13:27:26 +0100

    submitting modifications

--- scripts/python/publisher/publisher/__main__.py
+++ scripts/python/publisher/publisher/__main__.py
@@ -14,14 +14,14 @@ def cli():
     pass
 
 
-@click.command(name="Publish Blog", help="Publish the blog to the specified platforms.")
-@click.option("--platform", '-p', multiple=True, type=click.Choice(['hashnode', 'dev.to']), type=str, help="The platforms to publish the blog to.")
+@click.command("publish-blog", help="Publish the blog to the specified platforms.")
+@click.option("--platform", '-p', multiple=True, type=click.Choice(['hashnode', 'dev.to']), help="The platforms to publish the blog to.")
 @click.option("--blog", '-b', multiple=True, type=str, help="The slug names of the blogs to publish.")
 def publish_blog(platform: List):
     pass
 
 
-@click.command(name="Publish Collection", help="Publish a collection of blogs.")
+@click.command("publish-collection", help="Publish a collection of blogs.")
 @click.option("--collection", '-c', prompt=True, type=str, default=None, required=True, help="The name of the collection to publish.")
 def publish_collection(collection: str):
     if collection is None:
